a5028273966a3de22df882caed3088ddc0075945
cgpuglie <cgpuglie@asu.edu>
2017-03-26 19:09:14 -0700

    Added a worker resource

--- cluster.tf
+++ cluster.tf
@@ -4,8 +4,8 @@ provider "google" {
   region      = "us-west1"
 }
 
-resource "google_compute_instance" "default" {
-  name        = "swarm-manager1",
+resource "google_compute_instance" "manager1" {
+  name        = "manager1",
   description = "Docker Swarm manager",
   
   machine_type = "f1-micro",
@@ -17,5 +17,21 @@ resource "google_compute_instance" "default" {
   network_interface = {
     network    = "default",
     access_config = {}
+  }
+}
+
+resource "google_compute_instance" "worker1" {
+  name        = "worker1",
+  description = "Docker Swarm worker",
+  
+  machine_type = "f1-micro",
+  zone         = "us-west1-a",
+
+  disk = {
+    image      = "coreos-stable-1298-6-0-v20170315"
   },
+  network_interface = {
+    network    = "default",
+    access_config = {}
+  }
 }
