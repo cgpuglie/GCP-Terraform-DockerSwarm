e0fda66da6e50c3b50e73ff18d04d4604c6eb017
Cody Pugliese <cgpuglie@asu.edu>
2017-03-29 19:40:20 -0700

    Linted cluster.tf file

--- cluster.tf
+++ cluster.tf
@@ -1,76 +1,79 @@
 provider "google" {
-  credentials = "${ file("account.json") }",
-  project     = "${var.project_id}",
+  credentials = "${ file("account.json") }"
+  project     = "${var.project_id}"
   region      = "us-west1"
 }
 
 resource "google_compute_firewall" "swarm" {
-  name = "swarm",
-  description = "Docker swarm firewall rules",
-  network = "default",
+  name        = "swarm"
+  description = "Docker swarm firewall rules"
+  network     = "default"
 
   allow {
-    protocol = "tcp",
-    ports = ["2375", "2377", "7946"]
+    protocol = "tcp"
+    ports    = ["2375", "2377", "7946"]
   }
 
   allow {
-    protocol = "udp",
-    ports = ["4789", "7946"]
+    protocol = "udp"
+    ports    = ["4789", "7946"]
   }
 
   source_tags = ["manager", "worker"]
 }
 
 resource "google_compute_instance" "manager1" {
-  name        = "manager1",
-  description = "Docker Swarm manager",
-  
-  machine_type = "f1-micro",
-  zone         = "us-west1-a",
+  name        = "manager1"
+  description = "Docker Swarm manager"
+
+  machine_type = "f1-micro"
+  zone         = "us-west1-a"
 
   disk = {
-    image      = "coreos-stable-1298-6-0-v20170315"
-  },
+    image = "coreos-stable-1298-6-0-v20170315"
+  }
+
   network_interface = {
-    network    = "default",
+    network       = "default"
     access_config = {}
-  },
+  }
 
   tags = [
-    "manager"
-  ],
+    "manager",
+  ]
 
   connection {
-    type = "ssh",
-    user = "${var.remote_user}",
+    type        = "ssh"
+    user        = "${var.remote_user}"
     private_key = "${file("keys/id_rsa")}"
-  },
+  }
+
   provisioner "remote-exec" {
     inline = [
-      "sudo docker swarm init"
+      "sudo docker swarm init",
     ]
   }
 }
 
 resource "google_compute_instance" "worker1" {
-  name        = "worker1",
-  description = "Docker Swarm worker",
-  
-  machine_type = "f1-micro",
-  zone         = "us-west1-a",
+  name        = "worker1"
+  description = "Docker Swarm worker"
+
+  machine_type = "f1-micro"
+  zone         = "us-west1-a"
 
   disk = {
-    image      = "coreos-stable-1298-6-0-v20170315"
-  },
+    image = "coreos-stable-1298-6-0-v20170315"
+  }
+
   network_interface = {
-    network    = "default",
+    network       = "default"
     access_config = {}
-  },
+  }
 
   tags = [
-    "worker"
-  ],
+    "worker",
+  ]
 
   provisioner "local-exec" {
     command = <<-EOF
